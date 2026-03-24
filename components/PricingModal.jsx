ee63b0d108c076fcdaa7c09ac1ed709b9808d3e6
Utkarsh3904 <millenniumatom@gmail.com>
2026-03-24 23:17:01 +0530

    add PricingModal component for subscription tier display

---
+++ components/PricingModal.jsx
@@ -0,0 +1,35 @@
+"use client"
+
+import React, { useState } from 'react'
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogTitle, DialogTrigger } from './ui/dialog'
+import { Button } from './ui/button';
+import PricingSection from './PricingSection';
+
+const PricingModal = ({ subscriptionTier = "free", children }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const canOpen = subscriptionTier === "free";
+
+  return (
+    <div>
+       
+    <Dialog isOpen={isOpen} onOpenChange={canOpen ? setIsOpen : undefined }>
+      <form>
+        <DialogTrigger >
+            {children}
+        </DialogTrigger>
+        <DialogContent className="p-8 pt-4 sm:max-w-4xl">
+          
+          <DialogTitle/>
+
+          <PricingSection/>
+
+        </DialogContent>
+      </form>
+    </Dialog>
+
+    </div>
+  )
+}
+
+export default PricingModal
