82555078bc4e026cf8e890b53722c684e83ccd88
Utkarsh3904 <millenniumatom@gmail.com>
2026-04-01 12:52:41 +0530

    toaster added

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { neobrutalism } from '@clerk/ui/themes'
 
 import Header from "@/components/Header";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({subsets: ["latin"] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
 
           <Header/>
           <main className="min-h-screen"> {children} </main>
+          <Toaster richcolors/>
             <footer className="py-8 px-4 border-t">
               <div className="max-w-6xl mx-auto flex justify-center items-center" >
                 <p className="text-stone-500 text-sm">
